33175e764b5ba472e9a8fb0979bc19aa97298850
Xasanboy <xasanboymatvapayev@icloud.com>
2026-06-01 15:20:44 +0500

    Update Footer.tsx

--- frontend/src/ts/components/layout/footer/Footer.tsx
+++ frontend/src/ts/components/layout/footer/Footer.tsx
@@ -1,13 +1,11 @@
 import { JSXElement } from "solid-js";
 
 import { getIsScreenshotting } from "../../../states/core";
-import { showModal } from "../../../states/modals";
 import { getFocus } from "../../../states/test";
 import { cn } from "../../../utils/cn";
-import { Button } from "../../common/Button";
-import { Keytips } from "./Keytips";
-import { ThemeIndicator } from "./ThemeIndicator";
 import { VersionButton } from "./VersionButton";
+import { ThemeIndicator } from "./ThemeIndicator";
+import { Keytips } from "./Keytips";
 
 export function Footer(): JSXElement {
   return (
@@ -24,52 +22,8 @@ export function Footer(): JSXElement {
           "opacity-0": getFocus(),
         }}
       >
-        <div class="grid grid-cols-1 justify-items-start xs:grid-cols-2 sm:grid-cols-4 lg:flex">
-          <Button
-            variant="text"
-            text="aloqa"
-            fa={{
-              icon: "fa-envelope",
-              fixedWidth: true,
-            }}
-            onClick={() => showModal("Contact")}
-          />
-          <Button
-            variant="text"
-            text="qo'llab-quvvatlash"
-            fa={{
-              icon: "fa-donate",
-              fixedWidth: true,
-            }}
-            onClick={() => showModal("Support")}
-          />
-          <Button
-            variant="text"
-            text="github"
-            fa={{
-              icon: "fa-code",
-              fixedWidth: true,
-            }}
-            href="https://github.com/xasanboymatvafayev/monkeuy"
-          />
-          <Button
-            variant="text"
-            text="shartlar"
-            fa={{
-              icon: "fa-file-contract",
-              fixedWidth: true,
-            }}
-            href="/terms-of-service.html"
-          />
-          <Button
-            href="/privacy-policy.html"
-            variant="text"
-            text="maxfiylik"
-            fa={{
-              icon: "fa-lock",
-              fixedWidth: true,
-            }}
-          />
+        <div class="flex items-center gap-1 text-sub text-xs opacity-60">
+          © 2026 TezYoz
         </div>
         <div class="flex flex-col items-end text-right lg:flex-row">
           <ThemeIndicator />
